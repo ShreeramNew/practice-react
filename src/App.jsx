fed5c368ce946b13f5abb491c41c029b631f5f85
ShreeramNew <shreerambca1@gmail.com>
2024-03-09 15:18:52 +0530

    useRef practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useMemo, useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useMemo, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+   // //useMemo Practice
    const [num, setNum] = useState(0);
    const [newNum, setnewNum] = useState(0);
 
@@ -10,7 +12,7 @@ function App() {
    let newResult = useMemo(() => {
       let count = -1;
       let items = new Array(23873442);
-      console.log("I am working");
+      console.log("I am working");//This statement is not executed on each re-render
       items.fill(num);
       let newArray = items.map(() => {
          count = count + 1;
@@ -26,7 +28,20 @@ function App() {
 
    console.log(newResult);
    let end = Date.now();
-   console.log((end - start) / 1000, "Seconds");
+   console.log((end - start) / 1000, "Seconds");//To check time required for excecution
+
+
+   //useRef Practice
+   const [name,setName]=useState('');
+   let prevName=useRef(null)
+   let node=useRef(null)
+   console.log(node.current);
+   // const [prevName,setPrevName]=useState('');
+
+   function accessValue(){
+      prevName.current=name
+      setName(node.current.value);
+   }
 
    return (
       <>
@@ -36,6 +51,9 @@ function App() {
             <h2></h2>
             <button onClick={() => setnewNum((prev) => prev + 1)}>Change</button>
             <button onClick={() => setNum((prev) => prev + 1)}>Click</button>
+            <h1>Now your Name is {name}</h1>
+            <h1>But Your Name was {prevName.current}</h1>
+            <input ref={node} type="text" onChange={accessValue}/>
          </div>
       </>
    );
